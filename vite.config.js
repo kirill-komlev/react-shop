3fc4fd83583ef64e7ae4d6afa25e4cd8b2b4950e
kirill-komlev <141944537+kirill-komlev@users.noreply.github.com>
2025-10-28 10:45:57 +0000

    Добавил absolute imports

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	resolve: {
+		alias: {
+			src: '/src',
+			'app': '/src/app',
+			'pages': '/src/pages',
+			'widgets': '/src/widgets',
+			'features': '/src/features',
+			'entities': '/src/entities',
+			'shared': '/src/shared',
+		},
+	},
 })
