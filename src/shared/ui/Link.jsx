facc28cfb829829e1725bf5f9f2f3b957ed5901f
kirill-komlev <141944537+kirill-komlev@users.noreply.github.com>
2025-10-30 13:25:40 +0000

    Добавил hover для Link

--- src/shared/ui/Link.jsx
+++ src/shared/ui/Link.jsx
@@ -2,13 +2,22 @@ import { Link as RouterLink } from 'react-router'
 
 import { Link as MaterialLink } from '@mui/material'
 
-export function Link({ to, children }) {
+export function Link({ to, children, hover = true }) {
 	return (
 		<MaterialLink
 			component={RouterLink}
 			color='inherit'
 			underline='none'
 			to={to}
+			sx={
+				hover
+					? {
+							'&:hover': {
+								color: 'primary.main', // Color on hover
+							},
+					  }
+					: {}
+			}
 		>
 			{children}
 		</MaterialLink>
