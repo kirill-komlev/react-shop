386df1b0823be6d4958c97a8f0f387bae4a7cca1
Kirill Komlev <kirillkomlev02@gmail.com>
2025-11-07 16:14:22 +0300

    Улучшил кнопки добавления в корзину и удалил все лишнее

--- src/features/product-cart/ui/ProductCart.jsx
+++ src/features/product-cart/ui/ProductCart.jsx
@@ -2,9 +2,11 @@ import { IconButton, Tooltip } from '@mui/material'
 
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
 import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
 
 import { Button } from 'shared/ui/Button'
 import { useCartStore } from 'shared/libs/cart-store'
+import { useState } from 'react'
 
 export const AddCart = id => {
 	const addCart = useCartStore(state => state.addCart)
@@ -21,7 +23,14 @@ export const AddCart = id => {
 export const AddCartFull = id => {
 	const addCart = useCartStore(state => state.addCart)
 	let productId = id.id
-	return <Button onClick={() => addCart(productId)}>Купить</Button>
+	return (
+		<Button
+			onClick={() => addCart(productId)}
+			startIcon={<AddShoppingCartIcon />}
+		>
+			Купить
+		</Button>
+	)
 }
 
 export const DeleteCart = id => {
@@ -37,14 +46,19 @@ export const DeleteCart = id => {
 }
 
 export const DeleteCartFull = id => {
+	const [isHover, setIsHover] = useState(false)
 	const deleteCart = useCartStore(state => state.deleteCart)
 	let productId = id.id
 	return (
 		<Button
 			variant='outlined'
+			color={isHover ? 'error' : 'primary'}
+			startIcon={isHover ? <RemoveShoppingCartIcon /> : <ShoppingCartIcon />}
 			onClick={() => deleteCart(productId)}
+			onMouseOver={() => setIsHover(true)}
+			onMouseOut={() => setIsHover(false)}
 		>
-			В корзине
+			{isHover ? 'Убрать' : 'В корзине'}
 		</Button>
 	)
 }
