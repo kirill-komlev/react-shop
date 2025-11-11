e54a78cb7216fe627ee12d0bd320005b888ecca6
Kirill Komlev <kirillkomlev02@gmail.com>
2025-11-11 12:05:57 +0300

    Удалил ненужный код из шапки и слегка изменил её

--- src/widgets/header/ui/Header.jsx
+++ src/widgets/header/ui/Header.jsx
@@ -1,8 +1,7 @@
-import * as React from 'react'
+import { useState } from 'react'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Divider from '@mui/material/Divider'
-import Drawer from '@mui/material/Drawer'
 import IconButton from '@mui/material/IconButton'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
@@ -11,7 +10,7 @@ import ListItemText from '@mui/material/ListItemText'
 import MenuIcon from '@mui/icons-material/Menu'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import { Badge, Container, Menu, MenuItem, Stack, Tooltip } from '@mui/material'
+import { Badge, Container, Drawer, Menu, MenuItem, Stack, Tooltip } from '@mui/material'
 
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp'
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
@@ -19,6 +18,7 @@ import SportsEsportsIcon from '@mui/icons-material/SportsEsports'
 import GamepadIcon from '@mui/icons-material/Gamepad'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
 import FavoriteIcon from '@mui/icons-material/Favorite'
+import DeleteIcon from '@mui/icons-material/Delete'
 
 import { Button } from 'shared/ui/Button'
 import { Link } from 'shared/ui/Link'
@@ -29,10 +29,8 @@ import { APP_CONFIG } from 'shared/configs/app.config'
 import { CATEGORIES } from 'shared/configs/categories'
 
 import { capitalizeFirstLetter } from 'shared/libs/capitalizeFirstLetter'
-import { useFavoriteStore } from 'shared/libs/favorites-store'
-import { useCartStore } from 'shared/libs/cart-store'
+import { useFavoriteStore, useCartStore, useCartDrawerStore } from 'app/providers/store-provider/StoreProvider'
 
-const drawerWidth = 240
 const navItems = [
 	// ['Товары', PAGE_CONFIG.product],
 	['Акции', PAGE_CONFIG.sales],
@@ -52,17 +50,17 @@ const LinkButtonStyle = {
 }
 
 export function Header(props) {
-	const { window } = props
-	const [mobileOpen, setMobileOpen] = React.useState(false)
+	const [mobileOpen, setMobileOpen] = useState(false)
 
 	const favorite = useFavoriteStore(state => state.favorite)
 	const cart = useCartStore(state => state.cart)
+	const openCartDrawer = useCartDrawerStore(state => state.openCartDrawer)
 
 	const handleDrawerToggle = () => {
 		setMobileOpen(prevState => !prevState)
 	}
 
-	const [anchorEl, setAnchorEl] = React.useState(null)
+	const [anchorEl, setAnchorEl] = useState(null)
 	const open = Boolean(anchorEl)
 	const handleClick = event => {
 		setAnchorEl(event.currentTarget)
@@ -70,56 +68,6 @@ export function Header(props) {
 	const handleClose = () => {
 		setAnchorEl(null)
 	}
-
-	const drawer = (
-		<Box
-			onClick={handleDrawerToggle}
-			sx={{ px: 2 }}
-		>
-			<Typography
-				variant='h6'
-				sx={{ my: 2 }}
-			>
-				<Link
-					to={PAGE_CONFIG.home}
-					hover={false}
-				>
-					<Stack
-						direction='row'
-						spacing={1}
-						alignItems='center'
-					>
-						{/* <Box
-									component='img'
-									src={APP_CONFIG.logo}
-								/> */}
-						<GamepadIcon />
-						<Typography variant='h6'>{APP_CONFIG.name}</Typography>
-					</Stack>
-				</Link>
-			</Typography>
-			<Divider />
-			<List>
-				{navItems.map((item, index) => (
-					<ListItem
-						key={index}
-						disablePadding
-					>
-						<ListItemButton sx={{ px: 0 }}>
-							<Link
-								to={item[1]}
-								hover={false}
-							>
-								<ListItemText primary={item[0]} />
-							</Link>
-						</ListItemButton>
-					</ListItem>
-				))}
-			</List>
-		</Box>
-	)
-
-	const container = window !== undefined ? () => window().document.body : undefined
 
 	return (
 		<Box sx={{ display: 'flex' }}>
@@ -236,40 +184,24 @@ export function Header(props) {
 									</IconButton>
 								</Tooltip>
 							</Link>
-							<Link to={PAGE_CONFIG.cart}>
-								<Tooltip title='Корзина'>
-									<IconButton>
-										<Badge
-											max={9}
-											color='primary'
-											badgeContent={cart.length}
-										>
-											<ShoppingCartIcon />
-										</Badge>
-									</IconButton>
-								</Tooltip>
-							</Link>
+							<Tooltip
+								title='Корзина'
+								onClick={openCartDrawer}
+							>
+								<IconButton>
+									<Badge
+										max={9}
+										color='primary'
+										badgeContent={cart.length}
+									>
+										<ShoppingCartIcon />
+									</Badge>
+								</IconButton>
+							</Tooltip>
 						</Stack>
 					</Toolbar>
 				</Container>
 			</AppBar>
-			<nav>
-				<Drawer
-					container={container}
-					variant='temporary'
-					open={mobileOpen}
-					onClose={handleDrawerToggle}
-					ModalProps={{
-						keepMounted: true, // Better open performance on mobile.
-					}}
-					sx={{
-						display: { xs: 'block', sm: 'none' },
-						'& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-					}}
-				>
-					{drawer}
-				</Drawer>
-			</nav>
 		</Box>
 	)
 }
