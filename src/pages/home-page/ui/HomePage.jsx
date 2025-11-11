283669dae9dbef953960d2c90eb037a20aac4654
kirill-komlev <141944537+kirill-komlev@users.noreply.github.com>
2025-11-11 21:15:06 +0000

    Удалил ненужный box

--- src/pages/home-page/ui/HomePage.jsx
+++ src/pages/home-page/ui/HomePage.jsx
@@ -7,31 +7,26 @@ import { Button } from 'shared/ui/Button'
 
 export function HomePage() {
 	return (
-		<Box
-			component='section'
-			mt={12}
+		<Container
+			maxWidth='xl'
+			sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: 2 }}
 		>
-			<Container
-				maxWidth='xl'
-				sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: 2 }}
+			<Typography variant='h4'>Популярные товары</Typography>
+			<Grid
+				container
+				spacing={2}
+				width='100%'
 			>
-				<Typography variant='h4'>Популярные товары</Typography>
-				<Grid
-					container
-					spacing={2}
-					width='100%'
-				>
-					{DATA.slice(0, 4).map((item, index) => (
-						<Grid
-							size={{ xs: 12, sm: 6, md: 4, lg: 3 }}
-							key={index}
-						>
-							<ProductCard data={item} />
-						</Grid>
-					))}
-				</Grid>
-				<Button>Посмотреть все</Button>
-			</Container>
-		</Box>
+				{DATA.slice(0, 4).map((item, index) => (
+					<Grid
+						size={{ xs: 12, sm: 6, md: 4, lg: 3 }}
+						key={index}
+					>
+						<ProductCard data={item} />
+					</Grid>
+				))}
+			</Grid>
+			<Button>Посмотреть все</Button>
+		</Container>
 	)
 }
