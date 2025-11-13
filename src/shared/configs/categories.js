da414ea554ed33d128037ffc486c669be54febaa
Kirill Komlev <kirillkomlev02@gmail.com>
2025-11-13 14:15:53 +0300

    Убрал лишнюю константу и добавил фотографии для категорий

--- src/shared/configs/categories.js
+++ src/shared/configs/categories.js
@@ -2,17 +2,17 @@ export const CATEGORIES_FULL = {
 	mouses: {
 		en: ['mouse', 'mouses'],
 		ru: ['мышь', 'мыши'],
+		img: '/images/razer-viper-ultimate.jpg',
 	},
 
 	keyboards: {
 		en: ['keyboard', 'keyboards'],
 		ru: ['клавиатура', 'клавиатуры'],
+		img: '/images/logitech-g-pro-kb.jpg',
 	},
-	headphones: { en: ['headphones', 'headphones'], ru: ['наушники', 'наушники'] },
+	headphones: {
+		en: ['headphones', 'headphones'],
+		ru: ['наушники', 'наушники'],
+		img: '/images/steelseries-arctis-pro.jpg',
+	},
 }
-
-export const CATEGORIES = [
-	{ en: ['mouse', 'mouses'], ru: ['мышь', 'мыши'] },
-	{ en: ['keyboard', 'keyboards'], ru: ['клавиатура', 'клавиатуры'] },
-	{ en: ['', 'headphones'], ru: ['', 'наушники'] },
-]
