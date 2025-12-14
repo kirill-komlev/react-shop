223e2cee47b24b9adcd04c71c548bcf2797d54b3
Kirill Komlev <kirillkomlev02@gmail.com>
2025-12-15 00:07:18 +0300

    Рабочая версия фильтра от нейронки

--- src/widgets/product-filter/ui/ProductFilter.jsx
+++ src/widgets/product-filter/ui/ProductFilter.jsx
@@ -1,17 +1,204 @@
 import { Box, Checkbox, Collapse, Divider, Drawer, FormControlLabel, IconButton, List, ListItem, ListSubheader, Paper, Stack, Typography } from '@mui/material'
-
 import ExpandLess from '@mui/icons-material/ExpandLess'
 import ExpandMore from '@mui/icons-material/ExpandMore'
 import { FilterList } from '@mui/icons-material'
 import CloseIcon from '@mui/icons-material/Close'
-
-import { useState } from 'react'
+import { useState, useCallback, useMemo, memo } from 'react'
 import { DATA } from 'shared/configs/data'
 import { capitalizeFirstLetter } from 'shared/libs/capitalizeFirstLetter'
 import { Input } from 'shared/ui/Input'
 import { Button } from 'shared/ui/Button'
 import { useFilterSidebarStore } from 'app/providers/store-provider/StoreProvider'
 import { initialFilter } from 'shared/configs/filter'
+
+// Мемоизированный компонент для отрисовки контента фильтра
+const FilterContent = memo(
+	({ filter, onBrandToggle, onTypeToggle, onFilterChange, onPriceChange, onBooleanChange, onApply, onReset, isBrandOpen, isTypeOpen, uniqueBrands, uniqueType }) => {
+		// Обработчики для полей цены с useCallback
+		const handleMinPriceChange = useCallback(
+			e => {
+				onPriceChange(Number(e.target.value) || 0, filter.price[1])
+			},
+			[onPriceChange, filter.price[1]]
+		)
+
+		const handleMaxPriceChange = useCallback(
+			e => {
+				onPriceChange(filter.price[0], Number(e.target.value) || 0)
+			},
+			[onPriceChange, filter.price[0]]
+		)
+
+		return (
+			<Box>
+				<List
+					subheader={
+						<ListSubheader
+							component='div'
+							id='nested-list-subheader'
+						>
+							Цена
+						</ListSubheader>
+					}
+				>
+					<ListItem>
+						<Stack
+							direction='row'
+							spacing={2}
+						>
+							<Input
+								id='priceFrom'
+								size='small'
+								name='priceFrom'
+								label='От'
+								type='number'
+								placeholder='0'
+								value={filter.price[0]}
+								onChange={handleMinPriceChange}
+								key='price-from'
+							/>
+							<Input
+								id='priceTo'
+								size='small'
+								name='priceTo'
+								label='До'
+								type='number'
+								placeholder='10000'
+								value={filter.price[1]}
+								onChange={handleMaxPriceChange}
+								key='price-to'
+							/>
+						</Stack>
+					</ListItem>
+				</List>
+				<Divider />
+				{[
+					{
+						id: 'isRatingAbove4',
+						label: 'Рейтинг 4 и выше',
+					},
+					{
+						id: 'isDiscount',
+						label: 'По скидке',
+					},
+					{
+						id: 'isInStock',
+						label: 'В наличии',
+					},
+				].map((item, index) => (
+					<div key={index}>
+						<List>
+							<ListItem>
+								<FormControlLabel
+									control={
+										<Checkbox
+											id={item.id}
+											checked={filter[item.id]}
+											onChange={onBooleanChange}
+										/>
+									}
+									label={item.label}
+									sx={{ width: '100%' }}
+								/>
+							</ListItem>
+						</List>
+						<Divider />
+					</div>
+				))}
+
+				<List
+					subheader={
+						<ListSubheader
+							component='div'
+							id='nested-list-subheader'
+							sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', cursor: 'pointer' }}
+							onClick={onBrandToggle}
+						>
+							Бренд {isBrandOpen ? <ExpandLess /> : <ExpandMore />}
+						</ListSubheader>
+					}
+				>
+					<Collapse
+						in={isBrandOpen}
+						timeout='auto'
+					>
+						{uniqueBrands.sort().map(item => (
+							<ListItem key={item}>
+								<FormControlLabel
+									control={
+										<Checkbox
+											id='brand'
+											value={item}
+											checked={filter.brand.includes(item)}
+											onChange={onFilterChange}
+										/>
+									}
+									label={item}
+									sx={{ width: '100%' }}
+								/>
+							</ListItem>
+						))}
+					</Collapse>
+				</List>
+				<Divider />
+				<List
+					subheader={
+						<ListSubheader
+							component='div'
+							id='nested-list-subheader'
+							sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', cursor: 'pointer' }}
+							onClick={onTypeToggle}
+						>
+							Тип {isTypeOpen ? <ExpandLess /> : <ExpandMore />}
+						</ListSubheader>
+					}
+				>
+					<Collapse
+						in={isTypeOpen}
+						timeout='auto'
+					>
+						{uniqueType.sort().map(item => (
+							<ListItem key={item}>
+								<FormControlLabel
+									control={
+										<Checkbox
+											id='type'
+											value={item}
+											checked={filter.type.includes(item)}
+											onChange={onFilterChange}
+										/>
+									}
+									label={item}
+									sx={{ width: '100%' }}
+								/>
+							</ListItem>
+						))}
+					</Collapse>
+				</List>
+				<Divider />
+				<List>
+					<ListItem>
+						<Button
+							fullWidth
+							onClick={onApply}
+						>
+							Применить
+						</Button>
+					</ListItem>
+					<ListItem>
+						<Button
+							fullWidth
+							variant='text'
+							onClick={onReset}
+						>
+							Очистить
+						</Button>
+					</ListItem>
+				</List>
+			</Box>
+		)
+	}
+)
 
 export function ProductFilter({ category }) {
 	const [isBrandOpen, setIsBrandOpen] = useState(false)
@@ -21,255 +208,147 @@ export function ProductFilter({ category }) {
 	const filterSidebar = useFilterSidebarStore(state => state.filterSidebar)
 	const closeFilterSidebar = useFilterSidebarStore(state => state.closeFilterSidebar)
 
-	const data = DATA.filter(item => item.category === capitalizeFirstLetter(category))
+	// Мемоизация данных
+	const data = useMemo(() => DATA.filter(item => item.category === capitalizeFirstLetter(category)), [category])
 
-	const uniqueBrands = [...new Set(data.map(item => item.brand))]
-	const uniqueType = [...new Set(data.map(item => item.features.type))]
+	const uniqueBrands = useMemo(() => [...new Set(data.map(item => item.brand))], [data])
+	const uniqueType = useMemo(() => [...new Set(data.map(item => item.features.type))], [data])
 
-	const handleChange = e => {
-		const currentIndex = filter[e.target.id].indexOf(e.target.name)
-		const newFilter = [...filter[e.target.id]]
+	// Мемоизация обработчиков с useCallback
+	const handleChange = useCallback(
+		e => {
+			const currentIndex = filter[e.target.id].indexOf(e.target.value)
+			const newFilter = [...filter[e.target.id]]
 
-		if (currentIndex === -1) {
-			newFilter.push(e.target.name)
-		} else {
-			newFilter.splice(currentIndex, 1)
-		}
+			if (currentIndex === -1) {
+				newFilter.push(e.target.value)
+			} else {
+				newFilter.splice(currentIndex, 1)
+			}
 
-		setFilter({
-			...filter,
-			[e.target.id]: newFilter,
-		})
-	}
+			setFilter(prev => ({
+				...prev,
+				[e.target.id]: newFilter,
+			}))
+		},
+		[filter]
+	)
 
-	const handlePriceChange = (min, max) => {
-		if (max === 0 || min > max) {
-			max = 999999
-		}
+	const handlePriceChange = useCallback((min, max) => {
 		setFilter(prev => ({
 			...prev,
 			price: [min, max],
 		}))
-	}
+	}, [])
 
-	const handleBooleanChange = e => {
-		setFilter({
-			...filter,
-			[e.target.name]: e.target.checked,
-		})
-	}
+	const handleBooleanChange = useCallback(e => {
+		setFilter(prev => ({
+			...prev,
+			[e.target.id]: e.target.checked,
+		}))
+	}, [])
 
-	const onSubmit = () => {
+	const handleBrandToggle = useCallback(() => {
+		setIsBrandOpen(prev => !prev)
+	}, [])
+
+	const handleTypeToggle = useCallback(() => {
+		setIsTypeOpen(prev => !prev)
+	}, [])
+
+	const onSubmit = useCallback(() => {
 		console.log(filter)
-	}
+	}, [filter])
 
-	const onReset = () => {
+	const onReset = useCallback(() => {
 		setFilter(initialFilter)
-	}
+	}, [])
 
-	console.log('render')
-
-	const DrawerContent = () => (
-		<Box>
-			<List
-				subheader={
-					<ListSubheader
-						component='div'
-						id='nested-list-subheader'
-					>
-						Цена
-					</ListSubheader>
-				}
+	const MobileFilterSidebar = useMemo(
+		() => (
+			<Drawer
+				open={filterSidebar}
+				onClose={closeFilterSidebar}
+				anchor='right'
+				key='mobile-filter'
 			>
-				<ListItem>
+				<Box py={2}>
 					<Stack
 						direction='row'
-						spacing={2}
+						alignItems='center'
+						justifyContent='space-between'
+						px={2}
 					>
-						<Input
-							id='priceFrom'
-							size='small'
-							name='priceFrom'
-							label='От'
-							type='number'
-							placeholder='0'
-							onChange={e => handlePriceChange(Number(e.target.value), filter.price[1])}
-						/>
-						<Input
-							id='priceTo'
-							size='small'
-							name='priceTo'
-							label='До'
-							type='number'
-							placeholder='10000'
-							onChange={e => handlePriceChange(filter.price[0], Number(e.target.value))}
-						/>
+						<Typography variant='h4'>Фильтр</Typography>
+						<IconButton onClick={closeFilterSidebar}>
+							<CloseIcon />
+						</IconButton>
 					</Stack>
-				</ListItem>
-			</List>
-			<Divider />
-			{[
-				{
-					id: 'isRatingAbove4',
-					label: 'Рейтинг 4 и выше',
-				},
-				{
-					id: 'isDiscount',
-					label: 'По скидке',
-				},
-				{
-					id: 'isInStock',
-					label: 'В наличии',
-				},
-			].map((item, index) => (
-				<div key={index}>
-					<List>
-						<ListItem>
-							<FormControlLabel
-								control={
-									<Checkbox
-										id={item.id}
-										checked={filter[item.id]}
-										onChange={handleBooleanChange}
-									/>
-								}
-								label={item.label}
-								sx={{ width: '100%' }}
-							/>
-						</ListItem>
-					</List>
-					<Divider />
-				</div>
-			))}
-
-			<List
-				subheader={
-					<ListSubheader
-						component='div'
-						id='nested-list-subheader'
-						sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', cursor: 'pointer' }}
-						onClick={() => setIsBrandOpen(!isBrandOpen)}
-					>
-						Бренд {isBrandOpen ? <ExpandLess /> : <ExpandMore />}
-					</ListSubheader>
-				}
-			>
-				<Collapse
-					in={isBrandOpen}
-					timeout='auto'
-				>
-					{uniqueBrands.sort().map(item => (
-						<ListItem key={item}>
-							<FormControlLabel
-								control={
-									<Checkbox
-										id='brand'
-										value={item}
-										checked={filter.brand.includes(item)}
-										onChange={handleChange}
-									/>
-								}
-								label={item}
-								sx={{ width: '100%' }}
-							/>
-						</ListItem>
-					))}
-				</Collapse>
-			</List>
-			<Divider />
-			<List
-				subheader={
-					<ListSubheader
-						component='div'
-						id='nested-list-subheader'
-						sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', cursor: 'pointer' }}
-						onClick={() => setIsTypeOpen(!isTypeOpen)}
-					>
-						Тип {isTypeOpen ? <ExpandLess /> : <ExpandMore />}
-					</ListSubheader>
-				}
-			>
-				<Collapse
-					in={isTypeOpen}
-					timeout='auto'
-				>
-					{uniqueType.sort().map(item => (
-						<ListItem key={item}>
-							<FormControlLabel
-								control={
-									<Checkbox
-										id='type'
-										value={item}
-										checked={filter.type.includes(item)}
-										onChange={handleChange}
-									/>
-								}
-								label={item}
-								sx={{ width: '100%' }}
-							/>
-						</ListItem>
-					))}
-				</Collapse>
-			</List>
-			<Divider />
-			<List>
-				<ListItem>
-					<Button
-						fullWidth
-						onClick={onSubmit}
-					>
-						Применить
-					</Button>
-				</ListItem>
-				<ListItem>
-					<Button
-						fullWidth
-						variant='text'
-						onClick={onReset}
-					>
-						Очистить
-					</Button>
-				</ListItem>
-			</List>
-		</Box>
+					<FilterContent
+						filter={filter}
+						onBrandToggle={handleBrandToggle}
+						onTypeToggle={handleTypeToggle}
+						onFilterChange={handleChange}
+						onPriceChange={handlePriceChange}
+						onBooleanChange={handleBooleanChange}
+						onApply={onSubmit}
+						onReset={onReset}
+						isBrandOpen={isBrandOpen}
+						isTypeOpen={isTypeOpen}
+						uniqueBrands={uniqueBrands}
+						uniqueType={uniqueType}
+					/>
+				</Box>
+			</Drawer>
+		),
+		[
+			filterSidebar,
+			closeFilterSidebar,
+			filter,
+			isBrandOpen,
+			isTypeOpen,
+			uniqueBrands,
+			uniqueType,
+			handleBrandToggle,
+			handleTypeToggle,
+			handleChange,
+			handlePriceChange,
+			handleBooleanChange,
+			onSubmit,
+			onReset,
+		]
 	)
 
-	const MobileFilterSidebar = () => (
-		<Drawer
-			open={filterSidebar}
-			onClose={closeFilterSidebar}
-			anchor='right'
-		>
-			<Box py={2}>
-				<Stack
-					direction='row'
-					alignItems='center'
-					justifyContent='space-between'
-					px={2}
-				>
-					<Typography variant='h4'>Фильтр</Typography>
-					<IconButton onClick={closeFilterSidebar}>
-						<CloseIcon />
-					</IconButton>
-				</Stack>
-				<DrawerContent />
+	const DesktopFilter = useMemo(
+		() => (
+			<Box
+				component={Paper}
+				sx={{ width: '100%', maxWidth: 280, display: { xs: 'none', md: 'flex' } }}
+				key='desktop-filter'
+			>
+				<FilterContent
+					filter={filter}
+					onBrandToggle={handleBrandToggle}
+					onTypeToggle={handleTypeToggle}
+					onFilterChange={handleChange}
+					onPriceChange={handlePriceChange}
+					onBooleanChange={handleBooleanChange}
+					onApply={onSubmit}
+					onReset={onReset}
+					isBrandOpen={isBrandOpen}
+					isTypeOpen={isTypeOpen}
+					uniqueBrands={uniqueBrands}
+					uniqueType={uniqueType}
+				/>
 			</Box>
-		</Drawer>
-	)
-
-	const DesktopFilter = () => (
-		<Box
-			component={Paper}
-			sx={{ width: '100%', maxWidth: 280, display: { xs: 'none', md: 'flex' } }}
-		>
-			<DrawerContent />
-		</Box>
+		),
+		[filter, isBrandOpen, isTypeOpen, uniqueBrands, uniqueType, handleBrandToggle, handleTypeToggle, handleChange, handlePriceChange, handleBooleanChange, onSubmit, onReset]
 	)
 
 	return (
 		<>
-			<MobileFilterSidebar />
-			<DesktopFilter />
+			{MobileFilterSidebar}
+			{DesktopFilter}
 		</>
 	)
 }
