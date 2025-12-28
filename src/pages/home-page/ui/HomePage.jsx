74197cec315e4b9fbe0c547c7d52092b53d6e8ce
Kirill Komlev <kirillkomlev02@gmail.com>
2025-12-28 17:12:58 +0300

    Удалил ненужные части кода

--- src/pages/home-page/ui/HomePage.jsx
+++ src/pages/home-page/ui/HomePage.jsx
@@ -5,36 +5,7 @@ import { ProductCard } from 'widgets/product-card/ui/ProductCard'
 
 import { DATA } from 'shared/configs/data'
 import { Button } from 'shared/ui/Button'
-import Squares from 'shared/ui/Squares/Squares'
-import GridMotion from 'shared/ui/GridMotion/GridMotion'
 import Particles from 'shared/ui/Particles/Particles'
-
-const items = [
-	'Item 1',
-	<div key='jsx-item-1'>Custom JSX Content</div>,
-	'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-	'Item 2',
-	<div key='jsx-item-2'>Custom JSX Content</div>,
-	'Item 4',
-	<div key='jsx-item-2'>Custom JSX Content</div>,
-	'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-	'Item 5',
-	<div key='jsx-item-2'>Custom JSX Content</div>,
-	'Item 7',
-	<div key='jsx-item-2'>Custom JSX Content</div>,
-	'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-	'Item 8',
-	<div key='jsx-item-2'>Custom JSX Content</div>,
-	'Item 10',
-	<div key='jsx-item-3'>Custom JSX Content</div>,
-	'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-	'Item 11',
-	<div key='jsx-item-2'>Custom JSX Content</div>,
-	'Item 13',
-	<div key='jsx-item-4'>Custom JSX Content</div>,
-	'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-	'Item 14',
-]
 
 export function HomePage() {
 	return (
